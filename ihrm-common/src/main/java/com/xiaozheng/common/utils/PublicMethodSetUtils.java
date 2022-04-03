adace8cd5d7fd096e09aa0061b33483e40a4374f
Zong_Zh <1911298402@qq.com>
2022-04-03 22:27:02 +0800

    feat(social-security): 完善月度报表列表数据查询

---
+++ ihrm-common/src/main/java/com/xiaozheng/common/utils/PublicMethodSetUtils.java
@@ -0,0 +1,87 @@
+package com.xiaozheng.common.utils;
+
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.xiaozheng.model.dto.SsUserSocialSecurityDto;
+import com.xiaozheng.model.ss.SsArchiveDetailEntity;
+import com.xiaozheng.model.ss.SsCityPaymentItemEntity;
+import org.springframework.beans.BeanUtils;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * What -- 存放公共的方法
+ * <br>
+ * Describe --
+ * <br>
+ *
+ * @Package: com.xiaozheng.common.utils
+ * @ClassName: PublicMethodSetUtils
+ * @Author: 小政同学    QQ:xiaozheng666888@qq.com
+ * @CreateTime: 2022/4/3 19:00
+ */
+public class PublicMethodSetUtils {
+
+    /**
+     * 用户社保基础信息 和 社保-城市与缴费项目关联表 组装社保存档详细信息
+     * @param parameter
+     * @param groupCityPayment
+     * @param item
+     * @return 社保存档详细信息
+     */
+    public static  SsArchiveDetailEntity userSocialSecurityDtoToArchiveDetail(Map<String,Object> parameter, Map<String, List<SsCityPaymentItemEntity>> groupCityPayment, SsUserSocialSecurityDto item) {
+        BigDecimal socialSecurityCompanyPay = new BigDecimal(0);
+        BigDecimal socialSecurityPersonalPay = new BigDecimal(0);
+        SsArchiveDetailEntity ssArchiveDetailEntity = new SsArchiveDetailEntity();
+        BeanUtils.copyProperties(item, ssArchiveDetailEntity);
+        List<SsCityPaymentItemEntity> ssCityPaymentItemEntities = groupCityPayment.get(item.getParticipatingInTheCityId());
+        for (SsCityPaymentItemEntity cityPaymentItem : CollectionUtils.isNotEmpty(ssCityPaymentItemEntities) ? ssCityPaymentItemEntities : new ArrayList<SsCityPaymentItemEntity>()) {
+            // 企业是否缴纳开关，0为禁用，1为启用
+            if (cityPaymentItem.getSwitchCompany() == 1) {
+                BigDecimal augend;
+                if (cityPaymentItem.getPaymentItemId().equals("4") && item.getIndustrialInjuryRatio() != null) {
+                    augend = item.getIndustrialInjuryRatio().multiply(BigDecimal.valueOf(item.getSocialSecurityBase() != null ? item.getSocialSecurityBase() : 0));
+                } else {
+                    augend = cityPaymentItem.getScaleCompany().multiply(BigDecimal.valueOf(item.getSocialSecurityBase() != null ? item.getSocialSecurityBase() : 0));
+                }
+                BigDecimal divideAugend = augend.divide(new BigDecimal(100));
+                socialSecurityCompanyPay = socialSecurityCompanyPay.add(divideAugend);
+            }
+            // 个人是否缴纳开关，0为禁用，1为启用
+            if (cityPaymentItem.getSwitchPersonal() == 1) {
+                BigDecimal augend = cityPaymentItem.getScalePersonal().multiply(BigDecimal.valueOf(item.getSocialSecurityBase() != null ? item.getSocialSecurityBase() : 0));
+                BigDecimal divideAugend = augend.divide(new BigDecimal(100));
+                socialSecurityPersonalPay = socialSecurityPersonalPay.add(divideAugend);
+            }
+        }
+        ssArchiveDetailEntity.setYearsMonth(String.valueOf(parameter.get("yearsMonth")));
+        ssArchiveDetailEntity.setProportionOfPensionEnterprises(item.getEnterpriseProportion());
+        ssArchiveDetailEntity.setPersonalRatioOfProvidentFund(item.getPersonalProportion());
+        ssArchiveDetailEntity.setProvidentFundEnterprises(item.getEnterpriseProvidentFundPayment() != null ? item.getEnterpriseProvidentFundPayment() : new BigDecimal(0));
+        ssArchiveDetailEntity.setProvidentFundIndividual(item.getPersonalProvidentFundPayment() != null ? item.getPersonalProvidentFundPayment() : new BigDecimal(0));
+        ssArchiveDetailEntity.setTimeOfEntry(new SimpleDateFormat("yyyy-MM-dd").format(new Date()));
+
+        ssArchiveDetailEntity.setHouseholdRegistrationType(item.getHouseholdRegistration());
+        ssArchiveDetailEntity.setParticipatingInTheCity(item.getParticipatingInTheCity());
+        ssArchiveDetailEntity.setProvidentFundCity(item.getProvidentFundCity());
+        ssArchiveDetailEntity.setSocialSecurityNotes(item.getSocialSecurityNotes());
+        ssArchiveDetailEntity.setProvidentFundNotes(item.getProvidentFundNotes());
+        ssArchiveDetailEntity.setSocialSecurityBase(BigDecimal.valueOf(item.getSocialSecurityBase() != null ? item.getSocialSecurityBase() : 0));
+        ssArchiveDetailEntity.setProvidentFundBase(BigDecimal.valueOf(item.getProvidentFundBase() != null ? item.getProvidentFundBase() : 0));
+        ssArchiveDetailEntity.setAccumulationFundEnterpriseBase(BigDecimal.valueOf(item.getProvidentFundBase() != null ? item.getProvidentFundBase() : 0));
+        ssArchiveDetailEntity.setProportionOfProvidentFundEnterprises(item.getEnterpriseProportion());
+        ssArchiveDetailEntity.setIndividualBaseOfProvidentFund(BigDecimal.valueOf(item.getProvidentFundBase() != null ? item.getProvidentFundBase() : 0));
+        ssArchiveDetailEntity.setTotalProvidentFund(ssArchiveDetailEntity.getProvidentFundEnterprises().add(ssArchiveDetailEntity.getProvidentFundIndividual()));
+
+        ssArchiveDetailEntity.setSocialSecurity(socialSecurityCompanyPay.add(socialSecurityPersonalPay));
+        ssArchiveDetailEntity.setSocialSecurityEnterprise(socialSecurityCompanyPay);
+        ssArchiveDetailEntity.setSocialSecurityIndividual(socialSecurityPersonalPay);
+        ssArchiveDetailEntity.setSocialSecurityMonth(String.valueOf(parameter.get("yearsMonth")));
+        ssArchiveDetailEntity.setProvidentFundMonth(String.valueOf(parameter.get("yearsMonth")));
+        return ssArchiveDetailEntity;
+    }
+}
--- ihrm-social-security/src/main/java/com/xiaozheng/socialsecurity/service/ss/impl/SsArchiveDetailServiceImpl.java
+++ ihrm-social-security/src/main/java/com/xiaozheng/socialsecurity/service/ss/impl/SsArchiveDetailServiceImpl.java
@@ -1,21 +1,26 @@
 package com.xiaozheng.socialsecurity.service.ss.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.xiaozheng.common.utils.PageUtils;
-import com.xiaozheng.common.utils.Query;
-import com.xiaozheng.common.utils.ShiroContextUtils;
+import com.xiaozheng.common.utils.*;
+import com.xiaozheng.model.dto.SsUserSocialSecurityDto;
 import com.xiaozheng.model.ss.SsArchiveDetailEntity;
+import com.xiaozheng.model.ss.SsCityPaymentItemEntity;
+import com.xiaozheng.model.ss.SsUserSocialSecurityEntity;
 import com.xiaozheng.socialsecurity.dao.ss.SsArchiveDetailDao;
+import com.xiaozheng.socialsecurity.dao.ss.SsCityPaymentItemDao;
+import com.xiaozheng.socialsecurity.dao.ss.SsUserSocialSecurityDao;
 import com.xiaozheng.socialsecurity.service.ss.SsArchiveDetailService;
-import org.apache.poi.ss.formula.functions.T;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 社保-归档详情实现
@@ -26,6 +31,12 @@ import java.util.Map;
  */
 @Service("ssArchiveDetailService")
 public class SsArchiveDetailServiceImpl extends ServiceImpl<SsArchiveDetailDao, SsArchiveDetailEntity> implements SsArchiveDetailService {
+
+    @Autowired
+    private SsCityPaymentItemDao ssCityPaymentItemDao;
+
+    @Autowired
+    private SsUserSocialSecurityDao ssUserSocialSecurityDao;
 
     @Override
     public PageUtils<SsArchiveDetailEntity> queryPage(SsArchiveDetailEntity ssArchiveDetail, Map<String, Object> params) {
@@ -47,12 +58,39 @@ public class SsArchiveDetailServiceImpl extends ServiceImpl<SsArchiveDetailDao, 
     public PageUtils<SsArchiveDetailEntity> queryPageAndTran(SsArchiveDetailEntity ssArchiveDetail, Map<String, Object> params) {
         String companyId = ShiroContextUtils.getProfile().getCompanyId();
 
-        return new PageUtils(baseMapper.pageAndTran(
-                new Query<SsArchiveDetailEntity>().getPage(params),
-                ssArchiveDetail,
-                companyId
-        ));
+        boolean bCount = baseMapper.selectPage(new Page<>(1, 1), new QueryWrapper<SsArchiveDetailEntity>().eq("years_month", ssArchiveDetail.getYearsMonth())).getRecords().size() > 0;
+
+        if (bCount) {
+            // 有存档，直接根据年份和企业ID查询详情
+            return new PageUtils<>(baseMapper.pageAndTran(
+                    new Query<SsArchiveDetailEntity>().getPage(params),
+                    ssArchiveDetail,
+                    companyId
+            ));
+        } else {
+            // 没有存档，根据用户社保表、社保-城市与缴费项目关联表计算组装详细表数据
+            // 社保-城市与缴费项目关联表数据
+            List<SsCityPaymentItemEntity> cityPaymentItemList = ssCityPaymentItemDao.selectList(Wrappers.<SsCityPaymentItemEntity>lambdaQuery());
+            // 对数据进行分组，分组字段为城市ID
+            Map<String, List<SsCityPaymentItemEntity>> groupCityPayment = cityPaymentItemList.stream().collect(Collectors.groupingBy(SsCityPaymentItemEntity::getCityId));
+
+            // 构建社保详细表分页数据
+            Page<SsArchiveDetailEntity> archiveDetailEntityIPage = new Page<>();
+
+            //查询用户的社保列表 (用户和基本社保数据)
+            IPage<SsUserSocialSecurityDto> ssUserSocialSecurityDtoIPage = ssUserSocialSecurityDao.pageAndTran(new Query<SsUserSocialSecurityEntity>().getPage(params), new SsUserSocialSecurityEntity(), companyId);
+            BeanUtils.copyProperties(ssUserSocialSecurityDtoIPage, archiveDetailEntityIPage);
+
+            // 组装计算的结果集
+            List<SsArchiveDetailEntity> collect =
+                    ssUserSocialSecurityDtoIPage.getRecords().stream().map(item -> PublicMethodSetUtils.userSocialSecurityDtoToArchiveDetail(com.baomidou.mybatisplus.core.toolkit.BeanUtils.beanToMap(ssArchiveDetail), groupCityPayment,
+                            item)).collect(Collectors.toList());
+            archiveDetailEntityIPage.setRecords(collect);
+            return new PageUtils<>(archiveDetailEntityIPage);
+        }
     }
+
+
 
     /**
      * 根据年月获取统计存档详细信息
