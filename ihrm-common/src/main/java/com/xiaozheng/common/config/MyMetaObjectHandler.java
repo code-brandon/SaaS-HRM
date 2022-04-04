e269a11fa045e92b7a5af94af184af2a61479225
Zong_Zh <1911298402@qq.com>
2022-04-04 22:21:50 +0800

    fix(attendance): 修改Mybatis-plus的自定义字段填充

--- ihrm-common/src/main/java/com/xiaozheng/common/config/MyMetaObjectHandler.java
+++ ihrm-common/src/main/java/com/xiaozheng/common/config/MyMetaObjectHandler.java
@@ -1,12 +1,14 @@
 package com.xiaozheng.common.config;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.baomidou.mybatisplus.core.handlers.StrictFill;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author Xiao
@@ -18,14 +20,22 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
         log.info("start insert fill ....");
+        log.info(String.valueOf(new Date()));
         // 起始版本 3.3.0(推荐使用)
-        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        List<StrictFill<?, ?>> strictFills = new ArrayList<>();
+        strictFills.add(StrictFill.of("createDate", Date.class, new Date()));
+        strictFills.add(StrictFill.of("createTime", Date.class, new Date()));
+        this.strictInsertFill(findTableInfo(metaObject), metaObject,strictFills);
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         log.info("start update fill ....");
+        log.info(String.valueOf(new Date()));
         // 起始版本 3.3.0(推荐)
-        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+        List<StrictFill<?, ?>> strictFills = new ArrayList<>();
+        strictFills.add(StrictFill.of("updateDate", Date.class, new Date()));
+        strictFills.add(StrictFill.of("updateTime", Date.class, new Date()));
+        this.strictInsertFill(findTableInfo(metaObject), metaObject,strictFills);
     }
 }
