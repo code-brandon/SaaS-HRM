1c6a7bed9dedd06c2f2794f3ab45e6a70252af18
Zong_Zh <1911298402@qq.com>
2022-04-07 02:01:28 +0800

    feat(attendance): 添加Feign接口，通过用户ID查询用户详情单条数据
    调用 ihrm-employee 员工服务

--- ihrm-attendance/src/main/java/com/xiaozheng/attendance/feign/EmployeeApi.java
+++ ihrm-attendance/src/main/java/com/xiaozheng/attendance/feign/EmployeeApi.java
@@ -4,6 +4,8 @@ import com.xiaozheng.common.entity.R;
 import com.xiaozheng.model.em.EmUserCompanyPersonalEntity;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -32,4 +34,14 @@ public interface EmployeeApi {
     @ApiOperation("根据手机ID集合查询用户信息")
     @PostMapping("/em/personal/infosBymobile")
     R<List<EmUserCompanyPersonalEntity>> getByMobile(@RequestBody Set<String> mobiles);
+
+    /**
+     * 通过用户ID查询单条数据
+     *
+     * @param userId 主键
+     * @return 单条数据
+     */
+    @ApiOperation("通过用户ID查询用户详情单条数据")
+    @GetMapping("/em/personal/info/{userId}")
+    R<EmUserCompanyPersonalEntity> info(@PathVariable("userId") String userId);
 }
