b48611391fb06152b6740fd6bd8832bc3a394d63
Zong_Zh <1911298402@qq.com>
2022-04-19 18:24:32 +0800

    fix(audit): 修复排除SecurityAutoConfiguration仍然有权限校验问题

--- ihrm-audit/src/main/java/com/xiaozheng/audit/IhrmAuditApplication.java
+++ ihrm-audit/src/main/java/com/xiaozheng/audit/IhrmAuditApplication.java
@@ -4,7 +4,6 @@ import com.xiaozheng.common.config.DruidConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
@@ -13,7 +12,10 @@ import org.springframework.context.annotation.FilterType;
 /**
  * 排除 SecurityAutoConfiguration，因为activiti 7.x 以上 默认使用Security，而本项目使用的是Shiro
  */
-@SpringBootApplication(scanBasePackages = "com.xiaozheng",exclude ={ SecurityAutoConfiguration.class} )
+@SpringBootApplication(scanBasePackages = "com.xiaozheng",exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 @MapperScan("com.xiaozheng.audit.dao")
 // 排除自定义的DruidConfig
 @ComponentScan(excludeFilters= {@ComponentScan.Filter(type= FilterType.ASSIGNABLE_TYPE, value= { DruidConfig.class})})
