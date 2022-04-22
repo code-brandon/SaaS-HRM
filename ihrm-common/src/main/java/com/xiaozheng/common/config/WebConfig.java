610ecf6b4ad78ab271f7377a86e21c70f8f33c9d
Zong_Zh <1911298402@qq.com>
2022-04-22 20:35:05 +0800

    feat(common): 配置Jackson 序列化与反序列化
    解决：
    1. Long类型到前端 被JS处理 精度丢失
    2. 日期字符串无法反序列化为 LocalDateTime
    3. null字段，不序列化

--- ihrm-common/src/main/java/com/xiaozheng/common/config/WebConfig.java
+++ ihrm-common/src/main/java/com/xiaozheng/common/config/WebConfig.java
@@ -1,8 +1,17 @@
 package com.xiaozheng.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * What --
@@ -37,5 +46,32 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "OPTIONS", "DELETE")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    /**
+     * 配置Json
+     * @param builder
+     * @return
+     */
+    @Bean
+    public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        // 忽略value为null时key的输出
+        // objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        SimpleModule module = new SimpleModule();
+        /*
+          序列换成json时,将所有的long变成string
+          因为js中得数字类型不能包含所有的java long值
+         */
+        // 序列号
+        module.addSerializer(Long.class, ToStringSerializer.instance);
+        module.addSerializer(Long.TYPE, ToStringSerializer.instance);
+
+        // 反序列化
+        module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        objectMapper.registerModule(module);
+
+        return objectMapper;
     }
 }
