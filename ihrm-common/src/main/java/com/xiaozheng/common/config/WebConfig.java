be1692a395e783ea5261cc9439b2fe7df7539f7a
Zong_Zh <1911298402@qq.com>
2022-04-23 14:14:53 +0800

    fix(common): 修改Jackson 配置
    序列化包含 空值
    String类型null 序列化为 ”“

--- ihrm-common/src/main/java/com/xiaozheng/common/config/WebConfig.java
+++ ihrm-common/src/main/java/com/xiaozheng/common/config/WebConfig.java
@@ -1,6 +1,11 @@
 package com.xiaozheng.common.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -10,6 +15,7 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -57,14 +63,21 @@ public class WebConfig implements WebMvcConfigurer {
     public ObjectMapper jacksonObjectMapper(Jackson2ObjectMapperBuilder builder) {
         ObjectMapper objectMapper = builder.createXmlMapper(false).build();
         // 忽略value为null时key的输出
-        // objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+        // 解决序列号空String对象为Null，改为 ”“
+        objectMapper.getSerializerProvider().setNullValueSerializer(new JsonSerializer<Object>() {
+            @Override
+            public void serialize(Object value, JsonGenerator jg, SerializerProvider sp) throws IOException, JsonProcessingException {
+                jg.writeString("");
+            }
+        });
 
         SimpleModule module = new SimpleModule();
         /*
           序列换成json时,将所有的long变成string
           因为js中得数字类型不能包含所有的java long值
          */
-        // 序列号
+        // 序列化
         module.addSerializer(Long.class, ToStringSerializer.instance);
         module.addSerializer(Long.TYPE, ToStringSerializer.instance);
 
